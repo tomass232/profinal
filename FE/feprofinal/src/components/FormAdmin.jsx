4ee731cae61dc642445af3c6b98130d04a759adc
yancyalfaro1 <aalfaro@fwdcostarica.com>
2025-05-29 11:48:09 -0600

    commit

---
+++ FE/feprofinal/src/components/FormAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './FormAdmin.css';
+function FormAdmin() {
+  return (
+    <div>FormAdmin</div>
+  )
+}
+
+export default FormAdmin
--- FE/feprofinal/src/pages/Admin.jsx
+++ FE/feprofinal/src/pages/Admin.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import FormAdmin from '../components/FormAdmin';
+import './Admin.css';
 
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div className="admin-container">
+      <nav className="admin-nav">
+        <div className="logo">AdminPanel</div>
+        <div className="nav-links">
+          <a href="#">Dashboard</a>
+          <a href="#">My list</a>
+          <a href="#">Exports</a>
+        </div>
+      </nav>
 
-export default Admin
+      <div className="admin-content">
+        <h1 className="admin-title">Dashboard</h1>
+        <p className="admin-subtitle">Refine your search</p>
+
+        <div className="admin-body">
+          <FormAdmin />
+
+          <div className="admin-results">
+            <a href="#">450 results found</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
